45d8468456572b2e0d7e9608bdef951c1a88e23b
ramonruizdev <ramonruizrc@gmail.com>
2021-12-03 13:12:54 -0600

    Valor Futuro, Valor Presente, Interes dDel Periodo

---
+++ index.js
@@ -0,0 +1,56 @@
+// Valor Futuro
+
+let vf_r = document.getElementById("vf_resultado"); // Valor Futuro
+let vf_vp = document.getElementById("vf_vp"); // Valor Presente
+let vf_i = document.getElementById("vf_i"); //Interés del periodo
+let vf_n = document.getElementById("vf_n"); // Número de periodos
+
+let vf_btn = document.getElementById("vf_btn");
+
+function calcularVF() {
+  let vp_val = parseFloat(vf_vp.value);
+  let i_val = parseFloat(vf_i.value);
+  let n_val = parseFloat(vf_n.value);
+  let vf_val = vp_val * Math.pow(1 + i_val, n_val);
+  vf_r.innerHTML = `Valor Futuro = ${vf_val.toFixed(2)}`;
+}
+
+vf_btn.addEventListener("click", calcularVF);
+
+// Valor Presente
+
+let vp_r = document.getElementById("vp_resultado"); // Valor Presente Resultado
+let vp_vf = document.getElementById("vp_vf"); // Valor Futuro
+let vp_i = document.getElementById("vp_i"); //Interés del periodo
+let vp_n = document.getElementById("vp_n"); // Número de periodos
+
+let vp_btn = document.getElementById("vp_btn");
+
+function calcularVP() {
+  let vf_val = parseFloat(vp_vf.value);
+  let i_val = parseFloat(vp_i.value);
+  let n_val = parseFloat(vp_n.value);
+  let vp_val = vf_val / Math.pow(1 + i_val, n_val);
+  vp_r.innerHTML = `Valor Presente = ${vp_val.toFixed(2)}`;
+}
+
+vp_btn.addEventListener("click", calcularVP);
+
+// Interés del periodo
+
+let ip_r = document.getElementById("ip_resultado"); // Interés del periodo Resultado
+let ip_vf = document.getElementById("ip_vf"); // Valor Futuro
+let ip_vp = document.getElementById("ip_vp"); //Valor Presente
+let ip_n = document.getElementById("ip_n"); // Número de periodos
+
+let ip_btn = document.getElementById("ip_btn");
+
+function calcularIP() {
+  let vf_val = parseFloat(ip_vf.value);
+  let vp_val = parseFloat(ip_vp.value);
+  let n_val = parseFloat(ip_n.value);
+  let ip_val = [[vf_val / vp_val] - 1] / n_val;
+  ip_r.innerHTML = `Valor Presente = ${ip_val.toFixed(2)}`;
+}
+
+ip_btn.addEventListener("click", calcularIP);
